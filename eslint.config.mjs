60d3aba11cbd2a1bb0ae3daa28d4805bf14eb030
David Almeida <davyd.aguiar@hotmail.com>
2026-07-11 22:43:34 -0300

    🔧  Update configuration files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+
+import eslintConfigPrettier from "eslint-config-prettier";
 import nextTs from "eslint-config-next/typescript";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import prettier from "eslint-plugin-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  eslintConfigPrettier,
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
