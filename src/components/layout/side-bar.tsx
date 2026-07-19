91a3d08d90877854139f4ecac714ba66b227d618
David Almeida <davyd.aguiar@hotmail.com>
2026-07-19 02:28:46 -0300

    🔥 Format of Code: Land of Lamine Yamal, before the decisive game coding an appaco na mao

---
+++ src/components/layout/side-bar.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { LINKS_MENU } from "@/data/links-menu";
+import Link from "next/link";
+
+export const SideBar = () => {
+  return (
+    <aside className="hidden md:flex w-64 shrink-0 flex-col bg-violet-200/60 px-4 py-4">
+      <ul className="flex flex-col gap-2">
+        {LINKS_MENU.map((link) => (
+          <Link
+            key={link.id}
+            href={link.href}
+            className="flex flex-row justify-start items-center gap-2"
+          >
+            <Image
+              src={link.image}
+              width={32}
+              height={32}
+              className="w-10 h-10"
+              alt={link.alt}
+            />
+            <span className="text-sm text-violet-900 font-ubuntu font-medium">
+              {link.text}
+            </span>
+          </Link>
+        ))}
+      </ul>
+    </aside>
+  );
+};
---
+++ src/data/links-menu.ts
@@ -0,0 +1,30 @@
+export const LINKS_MENU = [
+  {
+    id: 1,
+    href: "/dashboard",
+    image: "/icons/home.png",
+    alt: "home",
+    text: "Inicio",
+  },
+  {
+    id: 2,
+    href: "/points",
+    image: "/icons/points.png",
+    alt: "points",
+    text: "Pontos",
+  },
+  {
+    id: 3,
+    href: "/rewards",
+    image: "/icons/rewards.png",
+    alt: "rewards",
+    text: "Resgates",
+  },
+  {
+    id: 4,
+    href: "/profile",
+    image: "/icons/profile.png",
+    alt: "profile",
+    text: "Perfil",
+  },
+];
