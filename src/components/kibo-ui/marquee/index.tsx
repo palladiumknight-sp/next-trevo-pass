7e68e0537b6b0b298b89dd5215af1cb7c2bd936c
David Almeida <davyd.aguiar@hotmail.com>
2026-07-19 00:16:58 -0300

    ✅ Update: Marquee component

--- src/components/kibo-ui/marquee/index.tsx
+++ src/components/kibo-ui/marquee/index.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import type { HTMLAttributes } from "react";
-import type { MarqueeProps as FastMarqueeProps } from "react-fast-marquee";
 import FastMarquee from "react-fast-marquee";
+import type { MarqueeProps as FastMarqueeProps } from "react-fast-marquee";
+import type { HTMLAttributes } from "react";
 import { cn } from "@/lib/utils";
 
 export type MarqueeProps = HTMLAttributes<HTMLDivElement>;
@@ -42,8 +42,8 @@ export const MarqueeFade = ({
   <div
     className={cn(
       "absolute top-0 bottom-0 z-10 h-full w-24 from-background to-transparent",
-      side === "left" ? "left-0 bg-gradient-to-r" : "right-0 bg-gradient-to-l",
-      className
+      side === "left" ? "left-0 bg-linear-to-r" : "right-0 bg-linear-to-l",
+      className,
     )}
     {...props}
   />
@@ -52,8 +52,5 @@ export const MarqueeFade = ({
 export type MarqueeItemProps = HTMLAttributes<HTMLDivElement>;
 
 export const MarqueeItem = ({ className, ...props }: MarqueeItemProps) => (
-  <div
-    className={cn("mx-2 flex-shrink-0 object-contain", className)}
-    {...props}
-  />
+  <div className={cn("mx-2 shrink-0 object-contain", className)} {...props} />
 );
